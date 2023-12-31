7bbb79d419f915ac650fd49ba31773ed2a23fb3a
Christian Duarte <w.christian.duarte@outlook.com>
2023-12-31 15:03:59 -0700

    Fixed bug back button to same route

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,13 +1,16 @@
-import React from "react";
-import { Link as LinkElem } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Link as LinkElem, useLocation } from "react-router-dom";
 
 interface Props extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
     to: string;
     untraceable?: boolean;
 }
 
-const Link = React.forwardRef(({to, untraceable, ...props}: Props, _) => (
-    <LinkElem to={{pathname: to}} state={untraceable ? {} : { prevPath: location.pathname }} {...props} />
-))
+const Link = React.forwardRef(({to, untraceable, ...props}: Props, _) => {
+    const location = useLocation();
+    return (
+        <LinkElem to={{pathname: to}} state={untraceable ? {} : { prevPath: to == location.pathname ? location.state.prevPath : location.pathname }} {...props} />
+    );
+})
 
 export default Link;
